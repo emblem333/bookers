ed40e6b6d6eb2817bd010fa5f82ef4e123b6b60c
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-02-08 11:40:42 +0000

    [Add]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,49 @@
 class BooksController < ApplicationController
   def new
+
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+    # 4. トップ画面へリダイレクト
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,4 @@
 class HomesController < ApplicationController
   def top
-    @list = List.find(params[:id])
   end
 end
