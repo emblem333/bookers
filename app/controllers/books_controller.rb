11e58f42fa76386e1e50c29d2bda0baa5b63f5ac
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-02-09 03:25:02 +0000

    [Finish]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -33,13 +34,14 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully updated."
   end
 
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
     flash[:notice] = "Book was successfully destroyed."
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+    redirect_to "/books"  # 投稿一覧画面へリダイレクト
   end
   private
   # ストロングパラメータ
